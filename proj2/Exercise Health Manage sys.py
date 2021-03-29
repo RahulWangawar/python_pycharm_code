e5a5a0279456bac0ade696fcdaef0de1a3440def
RahulWangawar <76062197+RahulWangawar@users.noreply.github.com>
2021-03-29 18:02:09 +0530

    Created health management system for already existing clients
    We have Client_list and Log_list .
    Target is to create .txt file for every client and their log....
    Also retrieve the data from .txt file

--- proj2/Exercise Health Manage sys.py
+++ proj2/Exercise Health Manage sys.py
@@ -1,36 +1,58 @@
-# l = 10 # Global
-#
-# def function1(n):
-#     # l = 5 #Local
-#     m = 8 #Local
-#     global l
-#     l = l + 45
-#     print(l, m)
-#     print(n, "I have printed")
-#
-# function1("This is me")
-# # print(m)
+# Health Management System
+# 3 clients - Rahul,Sony,Harry
+# Total 6 files
+# write a function that when executed takes as input client name
+# One more function to retrieve exercise or food for any client
 
-x = 89
+Client_list = {1: "Rahul", 2: "Sony", 3: "Harry"}
+Log_list = {1: "Exercise", 2: "Diet"}
 
 
-def harry():
-    x = 20
+def gettime():
+    import datetime
+    return datetime.datetime.now()
+# Data = 'y'
+# while Data !='n':
+try:
+    print("\nSelect Client name:")
+    for key, value in Client_list.items():
+        print("press", key, "for", value, "\n", end="")
+    client_name = int(input())
+    print("Selected", Client_list[client_name], "\n", end="")
 
-    def rohan():
-        global x
-        x = 88
+    print("press 1 to log")
+    print("press 2 to retrive")
+    do = int(input())
+    if do == 1:
+        print("Select the Log name:")
+        for key, value in Log_list.items():
+            print("press", key, "for", value, "\n", end="")
+        log_name = int(input())
+        print("Selected", Client_list[client_name], "for logging", Log_list[log_name], "\n", end="")
 
-    # print("before calling rohan()", x)
-    rohan()
-    print("after calling rohan()", x)
+        f = open(Client_list[client_name] + "_" + Log_list[log_name] + ".txt", "a")
+        k = 'y'
+        while (k != 'n'):
+            print("Enter", Log_list[log_name], "\n", end="")
+            mytext = input()
+            f.write("[" + str(gettime()) + "]" + mytext + "\n")
+            k = input("Add more?(y/n)")
+            continue
 
-
-harry()
-print(x)
-
-
-
-
-
+        f.close()
+    elif do == 2:
+        for key, value in Log_list.items():
+            print("press", key, "for", value, "\n", end="")
+        log_name = int(input())
+        print("Selected", Client_list[client_name], "for retriving", Log_list[log_name], "\n", end="")
+        f = open(Client_list[client_name] + "_" + Log_list[log_name] + ".txt", "rt")
+        content = f.readlines()
+        for line in content:
+            print(line, end="")
+        # Data = input("Want to continue ?? (y/n)")
+        f.close()
+    else:
+        print("Invalid input credentials")
+except Exception as e:
+    print("Invalid Input Credential")
 
